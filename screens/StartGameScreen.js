ba2f04654c9fb35eb502fb0a58c4876e7a536fb1
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-07-31 03:14:50 +0700

    4 - 16. Time to Finish the Confirmation Box

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,6 +11,7 @@ import {
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Colors from "../constants/colors";
+import NumberContainer from "../components/NumberContainer";
 
 const StartGameScreen = () => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -27,7 +28,7 @@ const StartGameScreen = () => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber >= 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
       Alert.alert(
         "Invalid number!",
         "Number has to be a number between 1 and 99",
@@ -41,9 +42,16 @@ const StartGameScreen = () => {
     setEnteredValue("");
   };
 
-  let confirmOutput;
+  let confirmedOutput;
+
   if (confirm) {
-    confirmOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" />
+      </Card>
+    );
   }
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -74,7 +82,7 @@ const StartGameScreen = () => {
             />
           </View>
         </Card>
-        {confirmOutput}
+        {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
   );
@@ -106,5 +114,9 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center",
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: "center",
   },
 });
