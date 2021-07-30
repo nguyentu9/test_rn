98c70de45d4ba2a812fc94479d1d8b7b1bf1ca4a
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-07-30 20:08:57 +0700

    4 - 4. Adding a Screen Component

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <StatusBar style="auto" />
       <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
